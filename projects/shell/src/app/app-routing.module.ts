9c550414dc93fcc5e6d8146def4e2e2ea9cecac4
Ahmad Almunajjed <ahmadalmunajjed@outlook.com>
2024-10-29 19:45:56 +0800

    finish off

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 
-const routes: Routes = [{
-  path: '',
-  component: HomeComponent
-}];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'remote',
+    loadChildren: () =>
+      loadRemoteModule('remote', './remoteModule').then((m) => m.RemoteMainModule)
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
