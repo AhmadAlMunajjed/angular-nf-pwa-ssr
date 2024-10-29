48646bf1bff97b334f51074d75718c08581a0864
Ahmad Almunajjed <ahmadalmunajjed@outlook.com>
2024-10-29 19:20:13 +0800

    add remote main module and home component to remote app

--- projects/remote/src/app/app-routing.module.ts
+++ projects/remote/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./remote-main/remote-main.module').then((m) => m.RemoteMainModule)
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
