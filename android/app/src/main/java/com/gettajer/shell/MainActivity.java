1383c432cdaf12b778555eecfa6d41774f42f103
Ahmad Almunajjed <ahmadalmunajjed@outlook.com>
2024-10-30 13:44:52 +0800

    install capacitor and add android native project

---
+++ android/app/src/main/java/com/gettajer/shell/MainActivity.java
@@ -0,0 +1,5 @@
+package com.gettajer.shell;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gettajer.shell',
+  appName: 'Shell App',
+  webDir: 'dist/shell/browser'
+};
+
+export default config;
